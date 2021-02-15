da3da48b8da7e4f2206674b2b0167623d6801a96
Dominik Winecki <dominikwinecki@gmail.com>
2021-02-15 18:47:25 -0500

    Insights change poc

--- screens/Insights.js
+++ screens/Insights.js
@@ -1,12 +1,16 @@
 // Libraries
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import _ from 'lodash';
+import { StyleSheet, Text, View, ActivityIndicator, StatusBar } from 'react-native';
 
 export default function Insights({ route }) {
   return (
     <View style={styles.container}>
-      <Text>Nothing to see here!</Text>
+
+      <Text style={{ fontWeight: 'bold', fontSize: 22 }}>African American</Text>
+      <StatusBar style="auto" />
+      <ActivityIndicator />
+      <Text>I'm here tpo</Text>
+
     </View>
   );
 }
@@ -15,7 +19,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
