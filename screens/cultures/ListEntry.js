798df93087cc98eaeb059f974b70b852a5419c6e
Anais Cabello <cabello.7@osu.edu>
2021-02-16 22:31:55 -0500

    Culture entries style created

--- screens/cultures/ListEntry.js
+++ screens/cultures/ListEntry.js
@@ -1,14 +1,58 @@
 // Libraries
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View , StyleSheet} from 'react-native';
+
+import { Ionicons } from '@expo/vector-icons'; 
+
+const styles = StyleSheet.create({
+    container:{
+        marginLeft: 10,
+        padding: 10,
+        paddingLeft: 0,
+        borderBottomColor: "rgba(0, 0, 0, 0.1)",
+        borderBottomWidth: 1,
+        flexDirection: "row",
+    },
+    titleContainer:{
+        justifyContent: "flex-start",
+        alignContent: "center",
+        flexDirection: "row",
+        flex: 1,
+    },
+    numInsightsContainer:{
+        width: 30,
+        backgroundColor: "#999",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignContent: "center",
+        flexDirection: "row",
+        padding: 3,
+    },
+    numInsightsText:{
+        color: "#FFFFFF"
+    },
+    iconContainer:{
+        width: 24,
+        marginLeft: 5,
+    },
+});
+
 
 export default function ListEntry({ culture }) {
     return (
-        <View>
-            <Text>{culture.title}</Text>
-            <Text>{culture.numInsights}</Text>
+        <View style={styles.container}>        
+            <View style={styles.titleContainer}>
+                <Text>{culture.title}</Text>
+            </View>
+            <View style={styles.numInsightsContainer}> 
+                <Text style={styles.numInsightsText}>{culture.numInsights}</Text>
+            </View>
+            <View style={styles.iconContainer}>
+                <Ionicons name="chevron-forward" size={20} color="#999" />
+            </View>
         </View>
+        
     );
 }
 
