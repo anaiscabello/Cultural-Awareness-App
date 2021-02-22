71c5a7196e201b3118656753f4bdc88faf9ca293
Dominik Winecki <dominikwinecki@gmail.com>
2021-02-22 16:38:17 -0500

    Just the components

--- screens/Insights.js
+++ screens/Insights.js
@@ -7,9 +7,40 @@ export default function Insights({ route }) {
     <View style={styles.container}>
 
       <Text style={{ fontWeight: 'bold', fontSize: 22 }}>African American</Text>
-      <StatusBar style="auto" />
-      <ActivityIndicator />
-      <Text>I'm here tpo</Text>
+
+      <Text style={{ color: 'lightgreen' }}>Trustworthy</Text>
+      <Text style={{ color: 'lightgrey', textAlign: 'right' }}>11/12 votes</Text>
+
+
+      <Text style={{ fontWeight: 'bold', color: 'lightgrey' }}>KEY INSIGHTS</Text>
+      <View
+        style={{
+          borderBottomWidth: 1,
+        }}
+      />
+
+      <Text>Key Insight 1</Text>
+      <View
+        style={{
+          borderBottomWidth: 1,
+        }}
+      />
+
+      <Text>Key Insight 2</Text>
+      <View
+        style={{
+          borderBottomWidth: 1,
+        }}
+      />
+
+      <Text>Our Actionable Brand solution offers game changers a suite of immersive offerings. We aim to globally engineer our architecture by strategically synergising our knowledge transfer competitive stakeholders. You need to dynamically invest your bandwidths to increase your best practice velocity. Our business grows organic growths to reliably and effectively incentivize our mobile stack.</Text>
+      <View
+        style={{
+          borderBottomWidth: 1,
+        }}
+      />
+
+      <Text style={{ fontWeight: 'bold', color: 'lightgrey' }}>SOURCES</Text>
 
     </View>
   );
